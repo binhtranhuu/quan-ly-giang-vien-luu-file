fc74aa63861142d214fd47027b2f4590ea2b396b
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-06-27 11:26:34 +0700

    2. Thiết kế giao diện trang chủ

---
+++ app/src/main/java/com/example/file_qlgv/ManAddGiangVien.java
@@ -0,0 +1,14 @@
+package com.example.file_qlgv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManAddGiangVien extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_add_giang_vien);
+    }
+}
---
+++ app/src/main/java/com/example/file_qlgv/ManChuyenMon.java
@@ -0,0 +1,14 @@
+package com.example.file_qlgv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManChuyenMon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_chuyen_mon);
+    }
+}
