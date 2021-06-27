eab781a267c534d30de6cbd79e5b32d8edbad855
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-06-27 11:45:34 +0700

    4. Thêm mới giảng viên

--- app/src/main/java/com/example/file_qlgv/ManAddGiangVien.java
+++ app/src/main/java/com/example/file_qlgv/ManAddGiangVien.java
@@ -2,13 +2,79 @@ package com.example.file_qlgv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.file_qlgv.model.B17DCAT023_GiangVien;
+import com.example.file_qlgv.storage.FileHelper;
+
+import java.util.ArrayList;
 
 public class ManAddGiangVien extends AppCompatActivity {
+
+    EditText edTenGV;
+    Button btnThemGV;
+    String ar1[] = {"Giao Su", "Tien Si", "Thac Si", "Cu Nhan"};
+    String ar2[] = {"1", "2", "3", "4", "5", "6"};
+    Spinner spTrinhDoGV, spKinhNghiemGV;
+
+    ArrayList<B17DCAT023_GiangVien> giangVienArrayList;
+
+    FileHelper fileHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_man_add_giang_vien);
+
+        fileHelper = new FileHelper(ManAddGiangVien.this);
+        giangVienArrayList = fileHelper.getData("giangvien.txt");
+
+        AnhXa();
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, ar1);
+        adapter1.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        spTrinhDoGV.setAdapter(adapter1);
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, ar2);
+        adapter2.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        spKinhNghiemGV.setAdapter(adapter2);
+
+        btnThemGV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                B17DCAT023_GiangVien giangVien = new B17DCAT023_GiangVien(
+                        giangVienArrayList.size() + 1,
+                        edTenGV.getText().toString().trim(),
+                        spTrinhDoGV.getSelectedItem().toString().trim(),
+                        spKinhNghiemGV.getSelectedItem().toString().trim()
+                );
+
+                giangVienArrayList.add(giangVien);
+
+                fileHelper.saveData(giangVienArrayList, "giangvien.txt");
+
+                Toast.makeText(ManAddGiangVien.this, "Thêm mới thành công", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(ManAddGiangVien.this, ManGiangVien.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void AnhXa() {
+        edTenGV = findViewById(R.id.edTenGV);
+        spTrinhDoGV = findViewById(R.id.spTrinhDoGV);
+        spKinhNghiemGV = findViewById(R.id.spKinhNghiemGV);
+        btnThemGV = findViewById(R.id.btnThemGV);
     }
 }
--- app/src/main/java/com/example/file_qlgv/storage/FileHelper.java
+++ app/src/main/java/com/example/file_qlgv/storage/FileHelper.java
@@ -1,4 +1,51 @@
 package com.example.file_qlgv.storage;
 
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 public class FileHelper {
+    private Context mcontext;
+
+    public FileHelper(Context context) {
+        this.mcontext = context;
+    }
+
+    public <T> void saveData(ArrayList<T> arrayList, String filename) {
+        try {
+            FileOutputStream fileOutputStream = mcontext.openFileOutput(filename, Context.MODE_PRIVATE);
+            ObjectOutputStream out = new ObjectOutputStream(fileOutputStream);
+            out.writeObject(arrayList);
+            out.close();
+            fileOutputStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public <T> ArrayList<T> getData(String filename) {
+        ArrayList<T> temp = new ArrayList<>();
+
+        try {
+            FileInputStream inputStream = mcontext.openFileInput(filename);
+            ObjectInputStream in = new ObjectInputStream(inputStream);
+            temp = (ArrayList<T>) in.readObject();
+            in.close();
+            inputStream.close();
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return temp;
+    }
 }
